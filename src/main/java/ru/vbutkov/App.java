70ff81e7895477b4bdfd67e5e9c98ecf43aadb54
melchior <v.bytkov@yandex.ru>
2024-08-10 17:35:38 +0300

    add local lib arrays-libs-1.0-SNAHSHOT.jar to project

--- src/main/java/ru/vbutkov/App.java
+++ src/main/java/ru/vbutkov/App.java
@@ -1,9 +1,14 @@
 package ru.vbutkov;
 
 import org.apache.commons.lang3.StringUtils;
+import ru.productstart.utils.ArrayUtils;
+
 
 public class App {
     public static void main(String[] args) {
         System.out.println(StringUtils.reverse("Hello word!"));
+
+        int result = ArrayUtils.binarySearch(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}, 8);
+        System.out.println(result);
     }
 }
