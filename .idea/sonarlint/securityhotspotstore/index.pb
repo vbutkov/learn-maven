0a28cdb620517a9baa12d928bd91b77a640e368a
melchior <v.bytkov@yandex.ru>
2024-08-10 15:34:15 +0300

    add Apache Commons Lang dependency and try it

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,3 +1,5 @@
 
 7
 pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+Q
+!src/main/java/ru/vbutkov/App.java,2/3/23ad7518585d26b1da5624e14bfa04215c519bc1
---
+++ src/main/java/ru/vbutkov/App.java
@@ -0,0 +1,9 @@
+package ru.vbutkov;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(StringUtils.reverse("Hello word!"));
+    }
+}
